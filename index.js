ab6001b1c05c11459c9b32216abd1763326e8ac0
Joseph Maruria <josephmaruria@gmail.com>
2022-05-18 17:32:51 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+const express = require('express');
+
+const app = new express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'pages/about.html'));
+});
+
+
+app.listen(4000, () => {
+    console.log('App listening on port 4000')
+});
